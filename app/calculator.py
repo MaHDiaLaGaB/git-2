89344f9907002107f44ab77dccb0e108e3e2ad56
Abdo ben yousef <abdomenyousef@gmail.com>
2025-10-20 19:56:35 +0200

    blbzt

--- app/calculator.py
+++ app/calculator.py
@@ -30,7 +30,7 @@ class Calculator:
         
         return x ** y
     
-    def sqr(self, x):
+    def int_division(self, x, y):
         """Calculate x to the power of y (** operation)."""
         
-        return x ** 0.5
+        return x // y
